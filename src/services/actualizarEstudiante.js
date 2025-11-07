4639bca494ad0dffb10cc5f17189b52f9e4feb94
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 01:12:03 -0300

    arreglos en Cursos

--- src/services/actualizarEstudiante.js
+++ src/services/actualizarEstudiante.js
@@ -1,9 +1,10 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
+const AUTH = import.meta.env.VITE_AUTH;
 
 export const actualizarEstudiante = async (id, data) => {
     const res = await fetch(`${BASE_URL}/estudiantes/${id}`, {
         method: "PUT",
-        headers: { "Content_Type": "application/json"},
+        headers: { "Content_Type": "application/json", Authorization: `${AUTH}`},
         body:JSON.stringify(data),
     });
     if (!res.ok) throw new Error("Error al actualizar estudiante");
