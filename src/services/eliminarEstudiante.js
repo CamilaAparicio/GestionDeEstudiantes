0bf47342759d8b0643d5e2926186cf3edf337e29
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 01:12:37 -0300

    arreglos en Cursos

--- src/services/eliminarEstudiante.js
+++ src/services/eliminarEstudiante.js
@@ -1,10 +1,11 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
+const AUTH = import.meta.env.VITE_AUTH;
 
 export const eliminarEstudiante = async (id) => {
     const res = await fetch(`${BASE_URL}/estudiantes/${id}`, {
         method: "DELETE",
          headers: {
-        "content-type": "application/json"},
+        "content-type": "application/json", Authorization: `${AUTH}`},
     });
     if (!res.ok) throw new Error("Error al eliminar estudiantee");
 }
