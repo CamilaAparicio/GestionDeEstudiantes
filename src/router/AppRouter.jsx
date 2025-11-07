c4d1418bc93444f1dd4c490a3329d1d64755f2a7
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 01:08:08 -0300

    arreglos en Cursos

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import {MenuLayout} from '../Layout/MenuLayout';
 import Home from '../pages/Home/Home';
-import {Cursos} from '../pages/Cursos/Cursos';
+import Cursos from '../pages/Cursos/Cursos';
 
 export const AppRouter = () => (
     <MenuLayout>
