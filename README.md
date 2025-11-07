75cbb525b6bd447408beb699e1c3217adf605d19
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 13:17:59 -0300

    correccion de rutas

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Bienvenido a StudiesApp
 
 ### StudiesApp es una web que organiza la información de los estudiantes.
+
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -15,7 +15,7 @@ export const AppRouter = () => (
             <Route path="/estudiantes/lista" element={<ListaEstudiantes />}/>
             <Route path="/estudiantes/agregar" element={<AddEstudiantePage />} />
             <Route path="/cursos" element={<EstudiantesPorCursoPage />} />
-            <Route path="/estudiantes/editar" element={<EditarEstudiantePage />} />
+            <Route path="/editar/:id" element={<EditarEstudiantePage />} />
         </Routes>
     </MenuLayout>
 )
