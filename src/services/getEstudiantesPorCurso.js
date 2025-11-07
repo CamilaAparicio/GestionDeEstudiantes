4c65b537f2c201d689ac11636322b84e6f1f2b02
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 02:25:56 -0300

    Corrección de GET

--- src/services/getEstudiantesPorCurso.js
+++ src/services/getEstudiantesPorCurso.js
@@ -1,7 +1,7 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getEstudiantesPorCurso = async (curso) => {
-    const res = await fetch(`${BASE_URL}/api/estudiantes?curso=${encodeURIComponent(curso)}`);
+    const res = await fetch(`${BASE_URL}api/estudiantes?curso=${encodeURIComponent(curso)}`);
     if(!res.ok) throw new Error ("Error al obtener estudiantes por curso");
     return res.json();
 }
